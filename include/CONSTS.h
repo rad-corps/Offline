fef071e2309326281a8bf42fde355d71914e2c93
rad-corps <hulbert.adam@gmail.com>
2015-05-04 18:54:35 +1000

    enemy attack sprite, doubled tile size

--- include/CONSTS.h
+++ include/CONSTS.h
@@ -1,7 +1,7 @@
 #pragma once
 
-#define TILE_SIZE 32
+#define TILE_SIZE 64
 
 #define INVADERS_FONT "./fonts/feisarv5.fnt"
-#define SCREEN_W 1024
-#define SCREEN_H 768
+#define SCREEN_W 1920
+#define SCREEN_H 1080
