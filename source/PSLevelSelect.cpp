3dbf222531a52b4052f3085a5aabb23416ff7483
rad-corps <hulbert.adam@gmail.com>
2015-05-12 21:40:50 +1000

    started building level select menu

--- source/PSLevelSelect.cpp
+++ source/PSLevelSelect.cpp
@@ -2,13 +2,35 @@
 
 #include "PSLevelSelect.h"
 #include "Vector.h"
+#include "DatabaseManager.h"
+#include "CONSTS.h"
+#include <string>
+#include "FrameworkHelpers.h"
+
+using namespace std;
 
 PSLevelSelect::PSLevelSelect()
 {
-	testText.SetText("Level Select");
+	cout << "1" << endl;
+ 	testText.SetText("Level Select");
 	testText.SetPos(Vector2(100,100));
 	testText.SetAlignment(ALIGN_CENTRE);
 
+cout << "2" << endl;
+
+	//get all the levels from the database
+	DatabaseManager dm;
+	char * error;
+cout << "2.5" << endl;
+	dm.Select(DB_STR, "tbl_level", "*", "", "", error);
+
+cout << "3" << endl;
+	for (int i = 0; i < dm.Rows(); ++i)
+	{
+		string levelID = ToString(dm.GetValueInt(i, "ID"));
+		cout << levelID << endl;
+	}
+cout << "4" << endl;
 }
 
 PSLevelSelect::~PSLevelSelect()
